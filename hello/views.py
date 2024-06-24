da1953b8133864e8cc9043c45afef1b3ec671838
araujofc <araujofc@hotmail.com>
2024-06-24 17:47:23 +0000

    decimoquinto commit

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Disciplina: Cloud Computing & Site Reliability Engineering Aluno: Fabricio Coutinho de Araujo")
+    return HttpResponse("<b>Disciplina:</b> Cloud Computing & Site Reliability Engineering <br><b>Aluno</b>: Fabricio Coutinho de Araujo")
